d5e3795cd1ee3225890fe32ec191db2cd40c8afb
Dyhan <dyhancrspo@gmail.com>
2020-06-24 10:57:55 +0700

    create Breadcrumb

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -33,37 +33,64 @@
 
 
 
-import React, { Component } from 'react'
-import {InputDate} from 'elements/Form/'
+// import React, { Component } from 'react'
+// import {InputDate} from 'elements/Form/'
 
-export default class Example extends Component {
-    state = {
-        value: {
-            startDate: new Date(),
-            endDate: new Date(),
-            key: "selection"
-          }
-    }
+// export default class Example extends Component {
+//     state = {
+//         value: {
+//             startDate: new Date(),
+//             endDate: new Date(),
+//             key: "selection"
+//           }
+//     }
 
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-      }
+//     handleChange = e => {
+//         this.setState({ [e.target.name]: e.target.value });
+//       }
     
 
-      render() {
-          return (
-              <div className="container">
-                <div className="row align-items-center justify-content-center" style={{height : "100vh"}}>
-                    <div className="col-auto">
-                        <InputDate
-                            max={30}
-                            onChange={this.handleChange}
-                            name="value"
-                            value={this.state.value}
-                        />
-                    </div>
-                  </div>
-              </div>
-          )
-      }
+//       render() {
+//           return (
+//               <div className="container">
+//                 <div className="row align-items-center justify-content-center" style={{height : "100vh"}}>
+//                     <div className="col-auto">
+//                         <InputDate
+//                             max={30}
+//                             onChange={this.handleChange}
+//                             name="value"
+//                             value={this.state.value}
+//                         />
+//                     </div>
+//                   </div>
+//               </div>
+//           )
+//       }
+// }
+
+
+
+import React, { Component } from "react";
+import Breadcrumb from "elements/Breadcrumb";
+
+export default class Example extends Component {
+  render() {
+    const breadcrumb = [
+      { pageTitle: "Home", pageHref: "" },
+      { pageTitle: "House Details", pageHref: "" }
+    ];
+
+    return (
+      <div className="container">
+        <div
+          className="row align-items-center justify-content-center"
+          style={{ height: "100vh" }}
+        >
+          <div className="col-auto">
+            <Breadcrumb data={breadcrumb} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
